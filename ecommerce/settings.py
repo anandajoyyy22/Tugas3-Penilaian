aa84d268d7489c87c09a799454aec60c7c9ff144
anandajoyyy22 <ananda.joy@ui.ac.id>
2024-12-26 07:36:51 +0700

    tugas 9 done

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,6 +40,8 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definitionn
 
 INSTALLED_APPS = [
+    'authentication',
+    'corsheaders',
     'main',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -53,6 +55,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -60,6 +63,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
    
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
+
 
 ROOT_URLCONF = 'ecommerce.urls'
 
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('authentication.urls')),
     path('', include('main.urls')),
 ]
 
